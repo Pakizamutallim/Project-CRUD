2dde53299ea58a573a66ba2cdaaf1e6897abf409
Pakizamutallim <127186291+Pakizamutallim@users.noreply.github.com>
2023-06-03 17:21:16 +0400

    Add files via upload

---
+++ Project CRUD-to cart updated/assets/js/index.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector(".menu")
+menu.addEventListener("click", function(){
+    const closedMenu =document.querySelector(".closed-menu")
+    closedMenu.style.display = "block"
+    
+    // if (closedMenu.style.display == "none") {
+    // }
+    // else if (closedMenu.style.display == "block") {
+    //     closedMenu.style.display = "none" 
+    // }
+})
+
+
+// menu.addEventListener("click", function(){
+//     const closedMenu =document.querySelector(".closed-menu")
+//     if (closedMenu.style.display == "block") {
+//         closedMenu.style.display = "none"
+//     }
+// })
